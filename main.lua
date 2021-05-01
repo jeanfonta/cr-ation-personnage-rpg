83df1e1abe3ff01127c2b71c7dae5d4754b650bf
jeanfonta <fontaine.j@outlook.com>
2021-05-01 23:44:57 +0200

    permet la ceation d'un personnage de rpg

---
+++ main.lua
@@ -0,0 +1,20 @@
+hero = {
+  nom = "Farador",
+  prenom ="karda",
+  vie = 50,
+  age = 25,
+  energie = 200,
+  classe = "mage",
+  magie_elementaire = "feu",
+  inventaire = {
+      arme = "baton",
+      livre = "grimoire",
+      piece_or = 20,
+      piece_argent = 50,
+      piece_bronce = 80,
+      armure = "robe de mage",
+      potion_de_sante = 2,
+      potion_de_mana = 10
+  }
+}
+print(hero.inventaire["livre"])
