d5e5b84200fa7f4b6db6e9db84175e1ae83ffa14
jeanfonta <fontaine.j@outlook.com>
2021-05-01 23:55:15 +0200

    rajout de contenu

--- main.lua
+++ main.lua
@@ -1,20 +1,21 @@
 hero = {
   nom = "Farador",
   prenom ="karda",
+  race_du_personnage = "elfs",
   vie = 50,
   age = 25,
   energie = 200,
   classe = "mage",
   magie_elementaire = "feu",
+  level = 1,
   inventaire = {
       arme = "baton",
       livre = "grimoire",
-      piece_or = 20,
-      piece_argent = 50,
-      piece_bronce = 80,
+      piece_en_or = 20,
+      piece_en_argent = 50,
+      piece_en_bronce = 80,
       armure = "robe de mage",
       potion_de_sante = 2,
       potion_de_mana = 10
   }
 }
-print(hero.inventaire["livre"])
